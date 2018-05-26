00365dd6c55ed29cd30f3747368ab8725d5d9cd9
Татьяна <tehfoxen@yandex.ru>
2018-05-26 22:38:01 +0300

    правки по критериям

--- build/js/script.js
+++ build/js/script.js
@@ -1,9 +1,7 @@
 
   var navMain = document.querySelector('.main-nav');
   var navToggle = document.querySelector('.main-nav__toggle');
-
   navMain.classList.remove('main-nav--nojs');
-
   navToggle.addEventListener('click', function() {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
